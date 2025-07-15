67b147d2b18b241c6e846a15a025546d0f424413
moureenmusimbi <musimbimoureen4@gmail.com>
2025-07-15 07:32:06 +0300

    my first commite

---
+++ my-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Hero.css';
+import profile from '../../assets/profile.png';
+
+const Hero = () => {
+    return ( <
+        div className = 'hero' >
+        <
+        img src = { profile }
+        alt = 'Profile'
+        style = {
+            {
+                width: '120px',
+                height: '120px',
+                borderRadius: '50%',
+                objectFit: 'cover',
+                border: '2px solid #ccc',
+                imageRendering: 'auto'
+            }
+        }
+        /> <
+        h1 >
+        <
+        span > I 'm Moureen Musimbi, </span>
+        a graphic designer & frontend developer based in Nairobi, Kenya. <
+        /h1> <
+        p >
+        I 'm Moureen Musimbi, a creative and detail-driven professional specializing in graphic design,
+        frontend development, and IT, passionate about turning ideas into impactful digital experiences. <
+        /p> <
+        div className = 'hero-action' >
+        <
+        div className = 'hero-connect' > Connect with me < /div> <
+        div className = 'hero-resume' > My resume < /div> <
+        /div> <
+        /div>
+    );
+};
+
+export default Hero;
